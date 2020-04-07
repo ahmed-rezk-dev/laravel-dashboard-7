f266ddd68afd3a06a954792ad1f47131c8ba0fb3
Ahmed Rezk <work72019@gmail.com>
2020-04-07 15:06:44 -0600

    Routes Translations.

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -11,70 +11,77 @@ use Session;
 
 class ContactUsController extends Controller
 {
-    #inbox page
+    //inbox page
     public function InboxPage()
     {
         $messages = Contact::latest()->get();
+
         return view('dashboard.inbox.inbox', compact('messages', $messages));
     }
 
-    #show message
+    //show message
     public function ShowMessage($id)
     {
         $message = Contact::findOrFail($id);
         $message->ShowOrNow = 1;
         $message->update();
+
         return view('dashboard.inbox.show_message', compact('message', $message));
     }
 
-    #send SMS
+    //send SMS
     public function SMS(Request $request)
     {
         $this->validate($request, [
-            'phone'       =>'required',
-            'sms_message' =>'required|min:1'
+            'phone' => 'required',
+            'sms_message' => 'required|min:1',
         ]);
 
         if (send_mobile_sms($request->phone, $request->sms_message)) {
             Session::flash('success', 'تم ارسال الرساله');
+
             return back();
         } else {
             Session::flash('warning', 'لم يتم ارسال الرساله ! ... تأكد من بيانات ال SMTP');
+
             return back();
         }
     }
 
-    #send EMAIL
+    //send EMAIL
     public function EMAIL(Request $request)
     {
         $this->validate($request, [
-            'email'=>'required',
-            'email_message' =>'required|min:1'
+            'email' => 'required',
+            'email_message' => 'required|min:1',
         ]);
 
-        #check if smtp congiration complete or no
+        //check if smtp congiration complete or no
         $checkConfig = SmsEmailNotification::first();
-        if ($checkConfig->smtp_type         == "" ||
-            $checkConfig->smtp_username     == "" ||
-            $checkConfig->smtp_password     == "" ||
-            $checkConfig->smtp_sender_email == "" ||
-            $checkConfig->smtp_port         == "" ||
-            $checkConfig->smtp_host         == ""
+        if ($checkConfig->smtp_type == '' ||
+            $checkConfig->smtp_username == '' ||
+            $checkConfig->smtp_password == '' ||
+            $checkConfig->smtp_sender_email == '' ||
+            $checkConfig->smtp_port == '' ||
+            $checkConfig->smtp_host == ''
         ) {
             Session::flash('danger', 'لم يتم ارسال الرساله ! .. يرجى مراجعة بيانات ال SMTP');
+
             return back();
         } else {
             Mail::to($request->email)->send(new MailMessage($request->email_message));
             Session::flash('success', 'تم ارسال الرساله');
+
             return back();
         }
     }
 
-    #delete mesage
+    //delete mesage
     public function DeleteMessage(Request $request)
     {
         Contact::findOrFail($request->id)->delete();
-        Session::flash('success', 'تم حذف الرساله');
+        Session::flash('success', __('titles.delete_message'));
+
         return redirect('admin/inbox-page');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,91 +4,65 @@ use Illuminate\Support\Facades\Route;
 
 /*---------------------------------Start Of FrontEnd--------------------------*/
 
-
-
-
 /*---------------------------------End Of FrontEnd--------------------------*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*---------------------------------Start Of DashBoard--------------------------*/
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'Manager', 'checkRole', 'smtpAndFcmConfig', 'Domain']], function () {
     /*Start Of DashBoard Controller (Intro Page)*/
     Route::get('dashboard', [
-        'uses'  => 'DashBoardController@Index',
-        'as'    => 'dashboard',
-        'icon'  => '<i class="icon-home4"></i>',
-        'title' => __('titles.home_page')
+        'uses' => 'DashBoardController@Index',
+        'as' => 'dashboard',
+        'icon' => '<i class="icon-home4"></i>',
+        'title' => __('titles.home_page'),
     ]);
-
-
-
 
     /*------------ Start Of ContactUsController ----------*/
-
-    #messages page
+    //messages page
     Route::get('inbox-page', [
         'uses' => 'ContactUsController@InboxPage',
-        'as'   => 'inbox',
+        'as' => 'inbox',
         'title' => __('titles.messages'),
         'icon' => '<i class="icon-inbox-alt"></i>',
-        'child' => ['showmessage', 'deletemessage', 'sendsms', 'sendemail']
+        'child' => ['showmessage', 'deletemessage', 'sendsms', 'sendemail'],
     ]);
 
-    #show message page
+    //show message page
     Route::get('show-message/{id}', [
         'uses' => 'ContactUsController@ShowMessage',
-        'as'  => 'showmessage',
-        'title' => 'عرض الرساله'
+        'as' => 'showmessage',
+        'title' => __('titles.show_message'),
     ]);
 
-    #send sms
+    //send sms
     Route::post('send-sms', [
         'uses' => 'ContactUsController@SMS',
-        'as'   => 'sendsms',
-        'title' => __('titles.by_sms')
+        'as' => 'sendsms',
+        'title' => __('titles.by_sms'),
     ]);
 
-    #send email
+    //send email
     Route::post('send-email', [
         'uses' => 'ContactUsController@EMAIL',
-        'as'   => 'sendemail',
-        'title' => 'ارسال Email'
+        'as' => 'sendemail',
+        'title' => __('titles.by_email'),
     ]);
 
-    #delete message
+    //delete message
     Route::post('delete-message', [
         'uses' => 'ContactUsController@DeleteMessage',
-        'as'   => 'deletemessage',
-        'title' => 'حذف الرساله'
+        'as' => 'deletemessage',
+        'title' => __('titles.delete_message'),
     ]);
 
     /*------------ End Of ContactUsController ----------*/
 
     /*------------ End Of UsersController ----------*/
 
-    #users list
+    //users list
     Route::get('users', [
         'uses' => 'UsersController@Users',
-        'as'   => 'users',
+        'as' => 'users',
         'title' => __('titles.users'),
         'icon' => '<i class="icon-vcard"></i>',
         'child' => [
@@ -100,105 +74,105 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'Manager', 'checkRol
             'notificationallusers',
             'sendcurrentemail',
             'sendcurrentsms',
-            'sendcurrentnotification'
-        ]
+            'sendcurrentnotification',
+        ],
     ]);
 
-    #add user
+    //add user
     Route::post('add-user', [
         'uses' => 'UsersController@AddUser',
-        'as'   => 'adduser',
-        'title' => 'اضافة عضو'
+        'as' => 'adduser',
+        'title' => __('titles.add_users'),
     ]);
 
-    #update user
+    //update user
     Route::post('update-user', [
         'uses' => 'UsersController@UpdateUser',
-        'as'   => 'updateuser',
-        'title' => 'تحديث عضو'
+        'as' => 'updateuser',
+        'title' => __('titles.update_users'),
     ]);
 
-    #delete user
+    //delete user
     Route::post('delete-user', [
         'uses' => 'UsersController@deleteUser',
-        'as'   => 'deleteuser',
-        'title' => 'حذف عضو'
+        'as' => 'deleteuser',
+        'title' => __('titles.delete_users'),
     ]);
 
-    #email for all users
+    //email for all users
     Route::post('email-users', [
         'uses' => 'UsersController@EmailMessageAll',
-        'as'   => 'emailallusers',
-        'title' => 'ارسال email للجميع'
+        'as' => 'emailallusers',
+        'title' => __('titles.send_email_to_all_users'),
     ]);
 
-    #sms for all users
+    //sms for all users
     Route::post('sms-users', [
         'uses' => 'UsersController@SmsMessageAll',
-        'as'   => 'smsallusers',
-        'title' => 'ارسال sms للجميع'
+        'as' => 'smsallusers',
+        'title' => __('titles.send_sms_to_all_users'),
     ]);
 
-    #notification for all users
+    //notification for all users
     Route::post('notification-users', [
         'uses' => 'UsersController@NotificationMessageAll',
-        'as'   => 'notificationallusers',
-        'title' => 'ارسال notification للجميع'
+        'as' => 'notificationallusers',
+        'title' => __('titles.sens_notifications_to_all_users'),
     ]);
 
-    #send email for current user
+    //send email for current user
     Route::post('send-current-email', [
         'uses' => 'UsersController@SendEmail',
-        'as'   => 'sendcurrentemail',
-        'title' => 'ارساله email لعضو'
+        'as' => 'sendcurrentemail',
+        'title' => __('titles.send_email_to_user'),
     ]);
 
-    #send sms for current user
+    //send sms for current user
     Route::post('send-current-sms', [
         'uses' => 'UsersController@SendSMS',
-        'as'   => 'sendcurrentsms',
-        'title' => 'ارساله sms لعضو'
+        'as' => 'sendcurrentsms',
+        'title' => __('titles.send_sms_to_user'),
     ]);
 
-    #send notification for current user
+    //send notification for current user
     Route::post('send-current-notification', [
         'uses' => 'UsersController@SendNotification',
-        'as'   => 'sendcurrentnotification',
-        'title' => 'ارساله notification لعضو'
+        'as' => 'sendcurrentnotification',
+        'title' => __('titles.sens_notifications_to_user'),
     ]);
     /*------------ End Of UsersController ----------*/
 
     /*------------ Start Of ReportsController ----------*/
 
-    #reports page
+    //reports page
     Route::get('reports-page', [
         'uses' => 'ReportsController@ReportsPage',
-        'as'   => 'reportspage',
+        'as' => 'reportspage',
         'title' => __('titles.reports'),
         'icon' => '<i class=" icon-flag7"></i>',
-        'child' => ['deleteusersreports', 'deletesupervisorsreports']
+        'child' => ['deleteusersreports', 'deletesupervisorsreports'],
     ]);
 
-    #delete users reports
+    //delete users reports
     Route::post('delete-users-reporst', [
         'uses' => 'ReportsController@DeleteUsersReports',
-        'as'   => 'deleteusersreports',
-        'title' => 'حذف تقارير الاعضاء'
+        'as' => 'deleteusersreports',
+        'title' => __('titles.delete_users_reports'),
     ]);
 
-    #delete supervisors reports
+    //delete supervisors reports
     Route::post('delete-supervisors-reporst', [
         'uses' => 'ReportsController@DeleteSupervisorsReports',
-        'as'   => 'deletesupervisorsreports',
-        'title' => 'حذف تقارير المشرفين'
+        'as' => 'deletesupervisorsreports',
+        'title' => __('titles.delete_administrations_reports'),
     ]);
     /*------------ End Of ReportsController ----------*/
 
     /*------------ start Of PermissionsController ----------*/
-    #permissions list
+    //permissions list
     Route::get('permissions-list', [
         'uses' => 'PermissionsController@PermissionsList',
-        'as'   => 'permissionslist',
+        'as' => 'permissionslist',
         'title' => __('titles.permissionslist'),
         'icon' => '<i class="icon-safe"></i>',
         'child' => [
@@ -206,44 +180,43 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'Manager', 'checkRol
             'addpermission',
             'editpermissionpage',
             'updatepermission',
-            'deletepermission'
-        ]
+            'deletepermission',
+        ],
     ]);
 
-    #add permissions page
+    //add permissions page
     Route::get('permissions', [
         'uses' => 'PermissionsController@AddPermissionsPage',
-        'as'   => 'addpermissionspage',
-        'title' => 'اضافة صلاحيه',
-
+        'as' => 'addpermissionspage',
+        'title' => __('titles.add_permission'),
     ]);
 
-    #add permission
+    //add permission
     Route::post('add-permission', [
         'uses' => 'PermissionsController@AddPermissions',
-        'as'   => 'addpermission',
-        'title' => 'تمكين اضافة صلاحيه'
+        'as' => 'addpermission',
+        'title' => __('titles.adding_permissions'),
     ]);
 
-    #edit permissions page
+    //edit permissions page
     Route::get('edit-permissions/{id}', [
         'uses' => 'PermissionsController@EditPermissions',
-        'as'   => 'editpermissionpage',
-        'title' => 'تعديل صلاحيه'
+        'as' => 'editpermissionpage',
+        'title' => __('titles.update_permissions'),
     ]);
 
-    #update permission
+    //update permission
     Route::post('update-permission', [
         'uses' => 'PermissionsController@UpdatePermission',
-        'as'   => 'updatepermission',
-        'title' => 'تمكين تعديل صلاحيه'
+        'as' => 'updatepermission',
+        'title' => __('titles.updating_permissions'),
     ]);
 
-    #delete permission
+    //delete permission
     Route::post('delete-permission', [
         'uses' => 'PermissionsController@DeletePermission',
-        'as'  => 'deletepermission',
-        'title' => 'حذف صلاحيه'
+        'as' => 'deletepermission',
+        'title' => __('titles.delete_permissions'),
     ]);
 
     /*------------ End Of PermissionsController ----------*/
@@ -251,40 +224,40 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'Manager', 'checkRol
     /*------------ Start Of MoneyAccountsController ----------*/
     Route::get('money-accounts', [
         'uses' => 'MoneyAccountsController@MoneyAccountsPage',
-        'as'   => 'moneyaccountspage',
+        'as' => 'moneyaccountspage',
         'icon' => '<i class="icon-cash3"></i>',
         'title' => __('titles.moneyaccountspage'),
-        'child' => ['moneyaccept', 'moneyacceptdelete', 'moneydelete']
+        'child' => ['moneyaccept', 'moneyacceptdelete', 'moneydelete'],
     ]);
 
-    #accept
+    //accept
     Route::post('accept', [
         'uses' => 'MoneyAccountsController@Accept',
-        'as'   => 'moneyaccept',
-        'title' => 'تأكيد معامله بنكيه',
+        'as' => 'moneyaccept',
+        'title' => __('titles.confrim_money_transaction'),
     ]);
 
-    #accept and delete
+    //accept and delete
     Route::post('accept-delete', [
         'uses' => 'MoneyAccountsController@AcceptAndDelete',
-        'as'   => 'moneyacceptdelete',
-        'title' => 'تأكيد مع حذف',
+        'as' => 'moneyacceptdelete',
+        'title' => __('titles.confirm_money_transaction_and_delete'),
     ]);
 
-    #delete
+    //delete
     Route::post('money-delete', [
         'uses' => 'MoneyAccountsController@Delete',
-        'as'   => 'moneydelete',
-        'title' => 'حذف معامله بنكيه',
+        'as' => 'moneydelete',
+        'title' => __('titles.delete'),
     ]);
     /*------------ End Of MoneyAccountsController ----------*/
 
     /*------------ Start Of SettingController ----------*/
 
-    #setting page
+    //setting page
     Route::get('setting', [
         'uses' => 'SettingController@Setting',
-        'as'   => 'setting',
+        'as' => 'setting',
         'title' => __('titles.settings'),
         'icon' => '<i class="icon-wrench"></i>',
         'child' => [
@@ -300,99 +273,99 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'Manager', 'checkRol
             'updatesitecopyright',
             'updateemailtemplate',
             'updategoogleanalytics',
-            'updatelivechat'
-        ]
+            'updatelivechat',
+        ],
     ]);
 
-    #add socials media
+    //add socials media
     Route::post('add-socials', [
         'uses' => 'SettingController@AddSocial',
-        'as'   => 'addsocials',
-        'title' => 'اضافة مواقع التواصل'
+        'as' => 'addsocials',
+        'title' => __('titles.adding_social_media'),
     ]);
 
-    #update socials media
+    //update socials media
     Route::post('update-socials', [
         'uses' => 'SettingController@UpdateSocial',
-        'as'   => 'updatesocials',
-        'title' => 'تحديث مواقع التواصل'
+        'as' => 'updatesocials',
+        'title' => __('titles.updating_social_media'),
     ]);
 
-    #delete social
+    //delete social
     Route::post('delete-social', [
         'uses' => 'SettingController@DeleteSocial',
-        'as'   => 'deletesocial',
-        'title' => 'حذف مواقع التاوصل'
+        'as' => 'deletesocial',
+        'title' => __('titles.deleting_social_media'),
     ]);
 
-    #update SMTP
+    //update SMTP
     Route::post('update-smtp', [
         'uses' => 'SettingController@SMTP',
-        'as'   => 'updatesmtp',
-        'title' => 'تحديث SMTP'
+        'as' => 'updatesmtp',
+        'title' => __('titles.updating_smtp'),
     ]);
 
-    #update SMS
+    //update SMS
     Route::post('update-sms', [
         'uses' => 'SettingController@SMS',
-        'as'   => 'updatesms',
-        'title' => 'تحديث SMS'
+        'as' => 'updatesms',
+        'title' => __('titles.updating_sms'),
     ]);
 
-    #update OneSignal
+    //update OneSignal
     Route::post('update-onesignal', [
         'uses' => 'SettingController@OneSignal',
-        'as'   => 'updateonesignal',
-        'title' => 'تحديث OneSignal'
+        'as' => 'updateonesignal',
+        'title' => __('titles.updating_oneSignal'),
     ]);
 
-    #update FCM
+    //update FCM
     Route::post('update-FCM', [
         'uses' => 'SettingController@FCM',
-        'as'   => 'updatefcm',
-        'title' => 'تحديث FCM'
+        'as' => 'updatefcm',
+        'title' => __('titles.updating_fcm'),
     ]);
 
-    #update SiteSetting
+    //update SiteSetting
     Route::post('update-sitesetting', [
         'uses' => 'SettingController@SiteSetting',
-        'as'   => 'updatesitesetting',
-        'title' => 'تحديث الاعدادات العامه'
+        'as' => 'updatesitesetting',
+        'title' => __('titles.updating_general_settings'),
     ]);
 
-    #update SEO
+    //update SEO
     Route::post('update-seo', [
         'uses' => 'SettingController@SEO',
-        'as'   => 'updateseo',
-        'title' => 'تحديث SEO'
+        'as' => 'updateseo',
+        'title' => __('titles.updating_seo'),
     ]);
 
-    #update footerCopyRight
+    //update footerCopyRight
     Route::post('update-sitecopyright', [
         'uses' => 'SettingController@SiteCopyRight',
-        'as'   => 'updatesitecopyright',
-        'title' => 'تحديث حقوق الموقع'
+        'as' => 'updatesitecopyright',
+        'title' => __('titles.updating_copyright'),
     ]);
 
-    #update email template
+    //update email template
     Route::post('update-emailtemplate', [
         'uses' => 'SettingController@EmailTemplate',
-        'as'   => 'updateemailtemplate',
-        'title' => 'تحديث قالب الايميل'
+        'as' => 'updateemailtemplate',
+        'title' => __('titles.updating_email_template'),
     ]);
 
-    #update api google analytics
+    //update api google analytics
     Route::post('update-google-analytics', [
         'uses' => 'SettingController@GoogleAnalytics',
-        'as'   => 'updategoogleanalytics',
-        'title' => 'تحديث google analytics'
+        'as' => 'updategoogleanalytics',
+        'title' => __('titles.updating_google_analytics'),
     ]);
 
-    #update api live chat
+    //update api live chat
     Route::post('update-live-chat', [
         'uses' => 'SettingController@LiveChat',
-        'as'   => 'updatelivechat',
-        'title' => 'تحديث live chat'
+        'as' => 'updatelivechat',
+        'title' => __('titles.updating_live_chat'),
     ]);
 
     /*------------ End Of SettingController ----------*/
@@ -401,8 +374,6 @@ Route::get('dd', function () {
     echo bcrypt(123456);
 });
 /*-------------------------------End Of DashBoard--------------------------------*/
-
-
 
 //Login Route
 Route::get('/login/', 'Auth\LoginController@showLoginForm')->name('login');
